b78d3796fa0d0c7a04d4ff7b32d2bb8ac3b182c9
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-12-13 10:28:03 +0545

    error handler

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 require("dotenv").config();
 const express = require("express");
-const connectDB = require("./db/connect");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.use(morgan("tiny"));
+app.use(express.json());
 
+app.use("/hero", (req, res) => {
+  res.send("hello there");
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB();
