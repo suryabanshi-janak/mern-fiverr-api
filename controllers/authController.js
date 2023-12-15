a491b6aff2d099fd4360f135354a21b3907dd5a3
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-12-15 22:12:07 +0545

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/User");
 
@@ -27,4 +29,45 @@ const register = asyncHandler(async (req, res) => {
   res.status(201).json({ message: "User has been created successfully!" });
 });
 
-module.exports = { register };
+// @desc   User login
+// route   POST /api/v1/auth/login
+// @access Public
+const login = asyncHandler(async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw new Error("Please provide a username and password");
+  }
+
+  const user = await User.findOne({ username });
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const matchPassword = await bcrypt.compare(user.password, password);
+  // if (!matchPassword) {
+  //   throw new Error("Invalid credentials!");
+  // }
+
+  const token = jwt.sign(
+    { userId: user._id, isSeller: user.isSeller },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+
+  const { password: userPassword, ...userData } = user._doc;
+
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+      expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
+    })
+    .status(200)
+    .json({ user: userData });
+});
+
+module.exports = { register, login };
